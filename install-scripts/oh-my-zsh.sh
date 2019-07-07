b437a9b6c9cb66be2b1bfcf75b32a9dabba4cf8e
Makram Jandar <makramjandar@gmail.com>
2019-07-07 04:16:38 +0200

    Update oh-my-zsh.sh

--- install-scripts/oh-my-zsh.sh
+++ install-scripts/oh-my-zsh.sh
@@ -1,57 +1,23 @@
-#自动化安装zsh,oh-my-zsh-tmux,powerline并配置powerline
+#zsh, tmux, oh-my-zsh, powerlevel10k, purepower
+
 echo "--- start update ---"
 sudo apt-get update
 echo "--- end update ---"
 
-echo "--- apt install zsh tmux python-pip ---"
-sudo yes Y | apt-get install zsh tmux python-pip
+echo "--- apt install zsh tmux---"
+sudo yes Y | apt-get install zsh tmux
 echo "--- end apt install ---"
 
 echo "--- install oh-my-zsh ---"
 yes Y | sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "--- end install ---"
 
-echo "--- install powerline ---"
-yes Y | su -c 'pip install git+git://github.com/Lokaltog/powerline'
+echo "--- install powerlevel10k ---"
+yes Y | git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
 echo "--- end install ---"
 
-echo "--- install powerline font ---"
-wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-sudo mv PowerlineSymbols.otf /usr/share/fonts/
-sudo fc-cache -vf
-sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+echo "--- install purepower ---"
+cd && curl -fsSLO https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.purepowersudo mv PowerlineSymbols.otf /usr/share/fonts/
+echo 'source ~/.purepower' >>! ~/.zshrc
 echo "--- end install powerline font ---"
-
-echo "--- append text to vimrc ---"
-sudo touch /etc/vim/vimrc
-sudo cat <<EOT>> /etc/vim/vimrc
-" for powerline
-set rtp+=/usr/local/lib/python2.7/dist-packages/powerline/bindings/vim/
-
-" Always show statusline
-set laststatus=2
-
-" Use 256 colours (Use this setting only if your terminal supports 256 colours)
-set t_Co=256"
-EOT
-echo "--- finished ---"
-
-echo "--- append text to zshrc ---"
-sudo touch /etc/zsh/zshrc
-sudo cat <<EOT>> /etc/zsh/zshrc
-# for powerline
-if [[ -r /usr/local/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh ]]; then
-    source /usr/local/lib/python2.7/dist-packages/powerline/bindings/zsh/powerline.zsh
-fi
-export TERM=xterm-256color
-EOT
-echo "--- finished ---"
-
-echo "--- append text to .tmux.conf ---"
-sudo touch ~/.tmux.conf
-sudo cat <<EOT>> ~/.tmux.conf
-source /usr/local/lib/python2.7/dist-packages/powerline/bindings/tmux/powerline.conf
-set-option -g default-terminal "screen-256color"
-set-option -g default-shell /bin/zsh
-EOT
-echo "--- finished ---"
