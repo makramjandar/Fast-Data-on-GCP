40bafd6b2f25d6848b62d59a6459e71274651c9e
Makram Jandar <makramjandar@gmail.com>
2019-07-07 04:33:22 +0200

    Update oh-my-zsh.sh

--- install-scripts/oh-my-zsh.sh
+++ install-scripts/oh-my-zsh.sh
@@ -12,6 +12,18 @@ echo "--- install oh-my-zsh ---"
 yes Y | sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 echo "--- end install ---"
 
+echo "--- install zsh-autosuggestions ---"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "--- end install ---"
+
+echo "--- install zsh-syntax-highlighting ---"
+git clone git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "--- end install ---"
+
+echo "--- install zsh-plugins ---"
+sed -i -e "s/plugins=(.*)/plugins=(git pip vi-mode dirhistory docker extract history command-not-found common-aliases zsh-autosuggestions zsh-syntax-highlighting)/g" .zshrc
+echo "--- end install ---"
+
 echo "--- install powerlevel10k ---"
 yes Y | git clone https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
@@ -20,4 +32,4 @@ echo "--- end install ---"
 echo "--- install purepower ---"
 cd && curl -fsSLO https://raw.githubusercontent.com/romkatv/dotfiles-public/master/.purepower
 echo 'source ~/.purepower' >>! ~/.zshrc
-echo "--- end install powerline font ---"
+echo "--- end install ---"
