18ae45828bfe472f8c59c516ee8c40ab9d9ad7e5
Makram Jandar <makramjandar@gmail.com>
2019-07-10 21:15:35 +0200

    +

---
+++ utils/install-gcloud-sdk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# @uthor and collected by Makram Jandar  _    _    _
+#    / \  / \  / \  / \  / \  / \  / \  / \  / \  / \
+#   ( G )( C )( L )( O )( U )( D )( - )( S )( D )( K )
+#    \_/  \_/  \_/  \_/  \_/  \_/  \_/  \_/  \_/  \_/
+#                                 © Install GCloud SDK
+
+sudo apt-get install curl && \
+sudo apt-get update && \
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" && \
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && \
+sudo curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+sudo apt-get update && sudo apt-get install google-cloud-sdk && \
+gcloud init --console-only && \
+gcloud config list
+
+# References
+# https://cloud.google.com/sdk/docs/downloads-apt-get
+
+# One-line install
+# wget -O - -q 'https://github.com/makramjandar/Fast-Data-on-GCP/blob/master/utils/install-gcloud-sdk.sh' | bash
