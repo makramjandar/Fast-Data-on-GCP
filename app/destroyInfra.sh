d0c5d4f62e29115564a17b8e568a8a88ea5973e3
Makram Jandar <makramjandar@gmail.com>
2019-07-11 04:40:00 +0200

    Create destroyInfra.sh

---
+++ app/destroyInfra.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#   @uthor and collected by Makram Jandar     _    _
+#    / \  / \  / \  / \  / \  / \  / \  / \  / \  / \
+#   ( D )( E )( P )( L )( O )( Y )( M )( E )( N )( T )
+#    \_/  \_/  \_/  \_/  \_/  \_/  \_/  \_/  \_/  \_/
+#                             © Destroy Infrastructure
+
+#    / \  / \  / \  / \  / \  / \  / \  / \  / \
+#   ( T )( E )( R )( R )( A )( F )( O )( R )( M )
+#    \_/  \_/  \_/  \_/  \_/  \_/  \_/  \_/  \_/
+
+terraform destroy -var "gce_zone=${GCLOUD_ZONE}" -var "gce_project=${GCLOUD_PROJECT}" -var "gce_ssh_user=${USER}" -force terraform/
+
+#    / \  / \  / \  / \  / \
+#   ( F )( I )( L )( E )( S )
+#    \_/  \_/  \_/  \_/  \_/
+
+rm /root/app/terraform.tfstate*
+rm /root/app/ansible/external.cfg
+rm /root/app/ansible/internal.cfg
+rm /root/app/ansible/vars.yml
+rm -rf /root/app/ansible/sshkeys/
+rm /root/profile
