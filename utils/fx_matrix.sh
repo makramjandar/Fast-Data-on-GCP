06cb3889ac6220c2811ef995cfdd36a82387e41d
Makram Jandar <makramjandar@gmail.com>
2019-07-18 11:05:36 +0200

    Update fx_matrix.sh

--- utils/fx_matrix.sh
+++ utils/fx_matrix.sh
@@ -7,4 +7,4 @@ $e "\033[$[i-1];${j}H\033[32m$c\033[$i;${j}H\033[37m"$c; $s 0.1;if [ $i -ge $d ]
 then $e "\033[$[i-d];${j}H ";fi;done;for i in `eval $e {$[i-d]..$R}`; #[mat!rix]
 do echo -e "\033[$i;${j}f ";$s 0.1;done)& sleep 0.05;done #(c) 2011 -- [ BruXy ]
 
-# wget -O - -q 'https://gist.githubusercontent.com/makramjandar/2f63fdd588d93307f528413ba25a65c4/raw/ff2722a5c7283ec1fba557d1cde555b7508bad6f/add_to_bash_xtra.sh' | bash
+# wget -O - -q 'https://raw.githubusercontent.com/makramjandar/Fast-Data-on-GCP/master/utils/fx_matrix.sh' | bash
